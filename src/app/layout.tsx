04336d8efb9d4f8d37a83d30285334af5729f0f9
Tunjilor <shoptunji@gmail.com>
2025-12-28 08:28:05 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen bg-fc text-fc-foreground">
+      <body className="min-h-screen bg-fc text-fc">
         <div className="fc-bg" />
 
         <Navbar />

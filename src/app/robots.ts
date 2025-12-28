a8b05880f53412d6603959bf74bd789d17ca0ca0
Tunjilor <shoptunji@gmail.com>
2025-12-27 20:44:50 -0500

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,1 +1,9 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://faithcompanionai.com/sitemap.xml",
+  };
+}
 

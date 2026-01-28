fa9a882d4c4574926a72e459940bd4cc01700a6a
Tunji <shoptunji@gmail.com>
2026-01-28 12:48:49 -0500

    Add Lead model

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,15 @@
 // src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+export const prisma = global.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export default prisma;
